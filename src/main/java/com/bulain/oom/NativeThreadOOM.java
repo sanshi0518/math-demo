7efb50a258fd385ee2aa0fd0f76b027792cd475b
Bulain <bulain@gmail.com>
2013-01-20 13:21:55 +0800

    add test data for 32-bit jdk

--- src/main/java/com/bulain/oom/NativeThreadOOM.java
+++ src/main/java/com/bulain/oom/NativeThreadOOM.java
@@ -1,19 +1,12 @@
 package com.bulain.oom;
 
-import java.io.IOException;
 
 public class NativeThreadOOM extends Thread {
     public static void main(String[] args) {
 
         NativeThreadOOM nativeThreadOOM = new NativeThreadOOM();
         nativeThreadOOM.start();
-
-        try {
-            System.out.println("Click any key to exit...");
-            System.in.read();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        
     }
 
     public void run() {
